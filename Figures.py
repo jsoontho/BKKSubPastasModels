f8c715099ab95ab5b813357fc36a1a9092919cb1
Jenny Soonthornrangsan <86302057+jsoontho@users.noreply.github.com>
2023-02-20 06:24:58 +0100

    Add files via upload
    Files are python scripts to run various models and produce various graphs

---
+++ Figures.py
@@ -0,0 +1,333 @@
+###############################################################################
+
+# Plotting figures for Groundwater Paper
+# Article Title: 
+
+# Jenny Soonthornrangsan 2023
+# TU Delft
+
+###############################################################################
+# import statements
+###############################################################################
+
+# Importing packages and libraries 
+import os
+import pandas as pd
+import numpy as np
+import pastas as ps
+import matplotlib.pyplot as plt
+import datetime
+
+# Changing current directory to locaiton of python script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Getting current directory path
+abs_path = os.path.dirname(__file__)
+
+# Importing script for pre-processing Thai GW data
+import main_functions as js_mfs
+
+#%%############################################################################
+# Plotting settings
+###############################################################################
+
+plt.rc('font', size=12) #controls default text size
+plt.rc('axes', titlesize=10) #fontsize of the title
+plt.rc('axes', labelsize=8) #fontsize of the x and y labels
+plt.rc('xtick', labelsize=8) #fontsize of the x tick labels
+plt.rc('ytick', labelsize=8) #fontsize of the y tick labels
+plt.rc('legend', fontsize=6) #fontsize of the legend
+
+#%% Pumping for four different sceniarios
+# Plotting pumping
+# Reading in data
+sheet = 'EstTotalPump_54-60_Int50'
+full_path = os.path.join(abs_path, 'inputs\BasinPumping.xlsx')
+pump_50 = pd.read_excel(full_path, sheet_name=sheet)
+sheet = 'EstTotalPump_54-60_IntF25'
+pump_25 = pd.read_excel(full_path, sheet_name=sheet)
+sheet = 'EstTotalPump_54-60_IntF100'
+pump_100 = pd.read_excel(full_path, sheet_name=sheet)
+sheet = 'EstTotalPump_54-60_IntF50_25'
+pump_50_25 = pd.read_excel(full_path, sheet_name=sheet)
+sheet = 'EstTotalPump_54-60_IntF0'
+pump_0 = pd.read_excel(full_path, sheet_name=sheet)
+
+# Plotting
+plt.figure(figsize=(3.2, 2.2), dpi=300)
+plt.plot(pump_50.Date, pump_50.Pump2, linewidth=1.5, label='500,000 m$^3$/day')
+plt.plot(pump_50.Date, pump_25.Pump2, linewidth=1.5, label='250,000 m$^3$/day')
+plt.plot(pump_50.Date, pump_50_25.Pump2, linewidth=1.5, label='Delayed\n250,000 m$^3$/day')
+plt.plot(pump_50.Date, pump_100.Pump2, linewidth=1.5, label='1,000,000 m$^3$/day')
+plt.plot(pump_50.Date, pump_0.Pump2, linewidth=1.5, label='No Pumping')
+plt.plot(pump_50.Date[:24472], pump_50.Pump2[:24472], linewidth=1.5, color="k")
+plt.legend()
+plt.grid(True, linestyle='dotted')
+ax = plt.gca()
+# ax.set_xticklabels(x.year)
+plt.xlabel("Year"); plt.ylabel("Pumping Rate (m$^3$/day)")
+# plt.title("Basin-Wide Pumping Estimates for Bangkok")
+
+## Saving
+path = "figures"
+fig_name = '1954-2060Pumping.png'
+full_figpath = os.path.join(path, fig_name)
+plt.savefig(full_figpath, bbox_inches='tight', format="png")
+
+#%% Pumping until 2020
+# Plotting pumping
+# Reading in data
+sheet = 'EstTotalPump_54-60_Int50'
+full_path = os.path.join(abs_path, 'inputs\BasinPumping.xlsx')
+pump_2020 = pd.read_excel(full_path, sheet_name=sheet)
+
+# Xticks
+x = pd.date_range(start=pump_2020.Date[0],
+                  end=pump_2020.Date[24472],
+                  periods=8)
+
+# Plotting
+plt.figure(figsize=(3.2, 2.2), dpi=300)
+plt.plot(pump_2020.Date[:24472], pump_2020.Pump2[:24472], linewidth=1.5)
+plt.xticks(x)
+ax = plt.gca()
+ax.set_xticklabels(x.year)
+from matplotlib.ticker import (AutoMinorLocator)
+ax.xaxis.set_minor_locator(AutoMinorLocator(3))
+plt.grid(True, linestyle=(0, (1, 5)), which='minor')
+plt.grid(True, linestyle="dotted", which='major')
+plt.xlabel("Year"); plt.ylabel("Pumping Rate (m$^3$/day)")
+# plt.title("Basin-Wide Pumping Estimates for Bangkok")
+
+## Saving
+path = "figures"
+fig_name = '1954-2020Pumping.png'
+full_figpath = os.path.join(path, fig_name)
+plt.savefig(full_figpath, bbox_inches='tight', format="png")
+
+#%% Plotting one-day block response
+modelpath = os.path.join(abs_path, 'models')
+# Model files
+modelfiles = os.listdir(modelpath)
+Wellnest_name = "LCBKK003"
+well_name = "NL34"
+wellmodel = [s for s in modelfiles \
+             if np.logical_and(Wellnest_name in s, well_name in s)][0]
+model = ps.io.load(modelpath + "\\" + wellmodel)
+
+# Block response
+b = model.get_block_response("well")*1000
+x = np.linspace(0, 100, 100)
+y = np.append(0, b)
+
+# Plotting
+plt.figure(figsize=(3.2, 2.2), dpi=300)
+plt.plot(x, y[:100], linewidth=1.5)
+plt.grid(True, linestyle='dotted')
+plt.xlabel("Days"); plt.ylabel("Groundwater Head (m)")
+# plt.title("Groundwater Response from \n1000 m$^3$/day of Pumping for One Day")
+
+## Saving
+path = "figures"
+fig_name = 'BlockResponseEx.png'
+full_figpath = os.path.join(path, fig_name)
+plt.savefig(full_figpath, bbox_inches='tight', format="png")
+
+#%% Plotting Groundwater forecasts
+modelpath = os.path.join(abs_path, 'models')
+# Model files
+modelfiles = os.listdir(modelpath)
+Wellnest_name = "LCBKK003"
+well_name = "NL34"
+wellmodel = [s for s in modelfiles \
+             if np.logical_and(Wellnest_name in s, well_name in s)][0]
+model = ps.io.load(modelpath + "\\" + wellmodel)
+pump_rfunc = ps.Gamma
+pumppath = os.path.join(abs_path, 'inputs\BasinPumping.xlsx')
+EstTotPump = pd.read_excel(full_path, sheet_name=sheet)
+pumpsheet = 'EstTotalPump_54-60_Int50'
+
+# Original pumping scenario 500,000 m3/day
+# Loading model and simulating based on new scenario
+time_min = '1988'; time_max = "2060";
+head50 = model.simulate(tmin=time_min, tmax=time_max)
+
+# Pumping scenario 250,000 m3/day
+# Loading model and simulating based on new scenario
+optiparam = model.parameters["optimal"]
+stdparam = model.parameters["stderr"]
+model.del_stressmodel("well")
+pumpsheet = 'EstTotalPump_54-60_IntF25'
+EstTotPump = pd.read_excel(pumppath, sheet_name=pumpsheet, index_col=0, parse_dates=['Date'])
+EstTotPump_ = ps.StressModel(EstTotPump.Pump, rfunc=pump_rfunc, name="well", settings="well", up=False)
+model.add_stressmodel(EstTotPump_)
+model.parameters["optimal"] = optiparam
+model.parameters["stderr"] = stdparam
+
+head25 = model.simulate(tmin=time_min, tmax=time_max)
+
+# Pumping scenario 1,000,000 m3/day
+# Loading model and simulating based on new scenario
+optiparam = model.parameters["optimal"]
+stdparam = model.parameters["stderr"]
+model.del_stressmodel("well")
+pumpsheet = 'EstTotalPump_54-60_IntF100'
+EstTotPump = pd.read_excel(pumppath, sheet_name=pumpsheet, index_col=0, parse_dates=['Date'])
+EstTotPump_ = ps.StressModel(EstTotPump.Pump, rfunc=pump_rfunc, name="well", settings="well", up=False)
+model.add_stressmodel(EstTotPump_)
+model.parameters["optimal"] = optiparam
+model.parameters["stderr"] = stdparam
+
+head100 = model.simulate(tmin=time_min, tmax=time_max)
+
+# Pumping scenario 500,000 to 250,000 m3/day
+# Loading model and simulating based on new scenario
+optiparam = model.parameters["optimal"]
+stdparam = model.parameters["stderr"]
+model.del_stressmodel("well")
+pumpsheet = 'EstTotalPump_54-60_IntF50_25'
+EstTotPump = pd.read_excel(pumppath, sheet_name=pumpsheet, index_col=0, parse_dates=['Date'])
+EstTotPump_ = ps.StressModel(EstTotPump.Pump, rfunc=pump_rfunc, name="well", settings="well", up=False)
+model.add_stressmodel(EstTotPump_)
+model.parameters["optimal"] = optiparam
+model.parameters["stderr"] = stdparam
+
+head50_25 = model.simulate(tmin=time_min, tmax=time_max)
+
+# Pumping scenario 0 m3/day
+# Loading model and simulating based on new scenario
+optiparam = model.parameters["optimal"]
+stdparam = model.parameters["stderr"]
+model.del_stressmodel("well")
+pumpsheet = 'EstTotalPump_54-60_IntF0'
+EstTotPump = pd.read_excel(pumppath, sheet_name=pumpsheet, index_col=0, parse_dates=['Date'])
+EstTotPump_ = ps.StressModel(EstTotPump.Pump, rfunc=pump_rfunc, name="well", settings="well", up=False)
+model.add_stressmodel(EstTotPump_)
+model.parameters["optimal"] = optiparam
+model.parameters["stderr"] = stdparam
+
+head0 = model.simulate(tmin=time_min, tmax=time_max)
+
+# Plotting
+plt.figure(figsize=(3.2, 2.2), dpi=300)
+plt.rc('xtick', labelsize=6) #fontsize of the x tick labels
+plt.rc('ytick', labelsize=6) #fontsize of the y tick labels
+plt.rc('legend', fontsize=5)
+plt.plot(head50, linewidth=1.5, label="500,000 m$^3$/day")
+plt.plot(head25, linewidth=1.5, label="250,000 m$^3$/day")
+plt.plot(head50_25, linewidth=1.5, label="Delayed\n250,000 m$^3$/day")
+plt.plot(head100, linewidth=1.5, label="1,000,000 m$^3$/day")
+plt.plot(head0, linewidth=1.5, label="No Pumping")
+plt.plot(head0[:12054], linewidth=1.5, color="black")
+plt.grid(True, linestyle='dotted')
+plt.legend()
+plt.xlabel("Years"); plt.ylabel("Groundwater Head (m)")
+# plt.title("LCBKK003 NL34\nGroundwater Forecasts\nBased on Pumping Scenarios")
+
+## saving
+path = "figures"
+fig_name = 'GWForecasts_LCBKK003NL34.png'
+full_figpath = os.path.join(path, fig_name)
+plt.savefig(full_figpath, bbox_inches='tight', format="png")
+
+
+#%% Data availability
+# Pumping vs groundwater vs levelling measurements 
+
+# Plotting pumping
+# Reading in data
+sheet = 'EstTotalPump_54-60_Int50'
+pumppath = 'inputs\BasinPumping.xlsx'
+pump_2020 = pd.read_excel(pumppath, sheet_name=sheet)
+
+# Xticks
+x = pd.date_range(start=pump_2020.Date[0],
+                  end=pump_2020.Date[24472],
+                  periods=8)
+
+# Plotting
+fig, axs = plt.subplots(3, sharex=True, figsize=(6.75, 3.38), dpi=300)
+axs[0].plot(pump_2020.Date[:24472], pump_2020.Pump2[:24472], linewidth=1.5)
+axs[0].set_ylabel("Pumping Rate\n(m$^3$/day)")
+axs[0].set_title("Basin-Wide Pumping Estimates for Bangkok")
+axs[0].grid(True, linestyle='dotted')
+# Plottign groundwater
+# Reading in groundwater data
+Wellnest_name = 'LCBKK003'
+well_name = 'BK2'
+well_path = "inputs\\"
+full_path = os.path.join(well_path, Wellnest_name + '.xlsx')
+data = pd.read_excel(full_path, skiprows=3)
+all_head_data, gw_well_head = js_mfs.GW_Data_Process(data, well_name)
+
+# CORRECTING GW HEAD DATA TO LAND SURFACE (COASTAL DEM 2.1)
+landsurf_path = os.path.join(well_path,
+                             'LandSurfElev_GWWellLocs.xlsx')
+
+# Each well nest has its own Ss and K sheet
+landsurf_data = pd.read_excel(landsurf_path, 
+                        sheet_name='2.1', 
+                        usecols="C:F",
+                        index_col=0) 
+
+gw_well_head.Head += (landsurf_data.RASTERVALU.loc[Wellnest_name])
+# Adding years and annual average heads
+gw_well_head["year"] = gw_well_head.index.year
+axs[1].plot(gw_well_head.index, gw_well_head.Head, color='k',
+            linewidth=1.5)
+axs[1].set_xlim(([datetime.date(1954, 1, 1), datetime.date(2020, 12, 31)]))
+axs[1].set_ylabel("Head (m)")
+axs[1].set_title("Groundwater Levels for Well Nest LCBKK003 and Well BK2")
+axs[1].grid(True, linestyle='dotted')
+loc = "inputs\SurveyingLevels.xlsx"
+
+subdata = pd.read_excel(loc, sheet_name=Wellnest_name+'_Leveling', 
+                    index_col = 3)
+subdata = pd.DataFrame(subdata)
+subdata.index = pd.to_datetime(subdata.index)
+
+# Getting rid of benchmarks outside time period
+subdata = subdata[(subdata.Year<=2020)]
+# subdata = subdata[(subdata.Year>=int(date.year[0])) & \
+#                   (subdata.Year<=int(date.year[-1]))]
+
+# Benchmarks should start at 0 at the first year. 
+bench = subdata.loc[:, subdata.columns.str.contains("Land")]
+bench = bench.fillna(0)
+
+if (bench.iloc[0] != 0).any():
+    bench.iloc[0] = 0;
+    
+## IMPORTANT INFO
+# For benchmark measurements, the first year is 0, the second year is 
+# the compaction rate over that first year. 
+# For implicit Calc, the first year has a compaction rate over that 
+# year, so to shift benchmarks value to the previouse year to match
+# Index has the right years
+bench.index = bench.index.shift(-1, freq='D')
+bench["date"] = bench.index
+
+# Gets the last date of each year
+lastdate = bench.groupby(pd.DatetimeIndex(bench['date']).year, 
+  as_index=False).agg({"date": max}).reset_index(drop=True)
+bench = bench.loc[lastdate.date]
+
+leveling = bench[
+    bench.columns[
+    bench.columns.str.contains("Land")].item()]
+
+leveling[ leveling==0 ] = np.nan
+axs[2].plot(leveling, 'o', color = "orange", linewidth=1.5,
+            markersize=3)
+axs[2].set_xlim(([datetime.date(1954, 1, 1), datetime.date(2020, 12, 31)]))
+axs[2].set_ylabel("Annual Rate\n(cm/year)")
+axs[2].set_title("Annual Land Subsidence Rates from Benchmark Leveling Station DMR62")
+plt.tight_layout()
+plt.rc('font', size=10) #controls default text size
+axs[2].grid(True, linestyle='dotted')
+
+## Saving
+path = "figures"
+fig_name = 'DataAvailability.png'
+full_figpath = os.path.join(path, fig_name)
+plt.savefig(full_figpath, bbox_inches='tight', format="png")
---
+++ Pastas_ResultsMaps_1950-2020.py
@@ -0,0 +1,358 @@
+###############################################################################
+
+# Plotting results from Pastas models for different wells in Bangkok, Thailand
+# A well nest may have 1-4 wells
+# Outputs: Spatial maps
+# Requires: previously created Pastas models
+
+# Jenny Soonthornrangsan 2023
+# TU Delft
+
+###############################################################################
+# import statements
+###############################################################################
+
+# Importing packages and libraries 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pastas as ps
+import os
+from mpl_toolkits.basemap import Basemap
+
+# Changing current directory to locaiton of python script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Getting current directory path
+abs_path = os.path.dirname(__file__)
+
+# Importing script for pre-processing Thai GW data
+import main_functions as js_mfs
+
+# Turning off warnings
+pd.options.mode.chained_assignment = None  # default='warn'
+
+#%%############################################################################
+# Plotting settings
+###############################################################################
+plt.rc('font', size=12) #controls default text size
+plt.rc('axes', titlesize=10) #fontsize of the title
+plt.rc('axes', labelsize=8) #fontsize of the x and y labels
+plt.rc('xtick', labelsize=8) #fontsize of the x tick labels
+plt.rc('ytick', labelsize=8) #fontsize of the y tick labels
+plt.rc('legend', fontsize=6) #fontsize of the legend
+#%% Importing Pastas
+
+###############################################################################
+# Pastas settings
+###############################################################################
+
+# Folder to save graph and import model 
+model_path = os.path.join(abs_path, 'models')
+fig_path = os.path.join(abs_path, 'figures')
+
+# Model files
+modelfiles = os.listdir(model_path)
+
+## Getting a list of all the wells
+# Input path
+input_path = 'inputs'
+
+# Total path
+tot_path = os.path.join(abs_path, input_path)
+
+files = os.listdir(tot_path)
+files = [i.replace('.xlsx', '') for i in files \
+         if i.startswith('LC') and "_" not in i]
+
+## Preallocation
+# Saving RMSE values for time period
+rmse = []; 
+
+# model time period
+time_min = '1950'; time_max = '2020'
+
+# Saving R2 values
+R2 = []; 
+
+# Saving gains 
+pump_A = []; precip_A = []
+
+# Saving SD of gains
+SDpump_A = []; SDprecip_A = [];
+
+# Saving results of RUNS test
+runs = [];
+
+# D constant
+d = []
+
+# step response tmax
+res_tmax = []
+
+# Range of obs
+obs_range = []
+
+# Creating subsets of wells that fit certain criteria
+subset_well_dict = {}
+
+###############################################################################
+# Pastas Import
+###############################################################################
+
+# For each well nest
+for Wellnest_name in files:
+    
+    # Reading in groundwater data
+    full_path = os.path.join(tot_path, Wellnest_name + '.xlsx')
+    data = pd.read_excel(full_path, skiprows=3)
+    
+    # For all wells in well nest
+    for wells in data.columns[-(len(data.columns)-2):]:
+        
+        # Well name in as a string
+        well_name = wells
+    
+        # Saving well nest and well name association
+        subset_well_dict[well_name] = [Wellnest_name]
+        
+        # If model file exists:
+        try:
+            wellmodel = [s for s in modelfiles \
+                         if np.logical_and(Wellnest_name in s, well_name in s)][0]
+            model = ps.io.load(model_path + "\\" + wellmodel)
+            
+            # If does not exist
+        except FileNotFoundError:
+            print("No model for " + Wellnest_name + "_" + well_name)
+            continue
+        
+        # Noise model
+        noise = model.noise()
+                    
+        # Runs test
+        stat, p = ps.stats.runs_test(noise)
+        
+        # Saving rmse  
+        # Normalizing RMSE to max-min of data
+        max_obs = max(model.observations())
+        min_obs = min(model.observations())
+        rmse_rel = model.stats.rmse(tmin=time_min, tmax=time_max)/(max_obs-min_obs)
+        
+        # Saving relative rmse
+        rmse.append(Wellnest_name)
+        rmse.extend((well_name, rmse_rel))
+        
+        # Saving pumping step response tmax
+        res_tmax.append(Wellnest_name)
+        res_tmax.extend((well_name, model.get_response_tmax("well", 
+                                                            cutoff=0.90)/365))
+
+        # Saving range of obs for each model
+        obs_range.append(Wellnest_name)
+        obs_range.extend((well_name, model.observations().max() - \
+                          model.observations().min()))
+        
+#%%############################################################################
+# Relative RMSE Plotting
+###############################################################################
+# Output: One graph, 2D plot with color indicator of RMSE for all 4 aq 
+# RMSE is relative RMSE (relative to observation range)
+
+# Importing spatial coordinates
+full_path = os.path.join(tot_path, 'GroundwaterWellLocs.xls')
+gwwell_locs = pd.read_excel(full_path)
+
+# Locations of wellnests; removing duplicates
+gwwell_locs = gwwell_locs.drop_duplicates('WellNest_Name', keep='first')
+
+# Aquifer of interest
+aqs = ["BK", "PD", "NL", "NB"]
+
+## Preallocation
+# Empty dictionary
+d_dict = {}
+
+# For each aquifer of interest
+for aq in aqs: 
+    
+    ## Preallocation
+    # Saving relevant xs, ys, and rmse
+    xs = []; ys = []; cs = [];
+    # Subsets of the RMSE data, and list of wellnest names
+    well_data = []; wellnest_list = [];
+    
+    ## Extracting relavent data
+    # Going through each rmse saved
+    for i in rmse:
+        
+        # If string ie if the well nest or aquifer name
+        if type(i) == str:
+            
+            # If the aquifer anem
+            if i.startswith(aq):
+        
+                # Saving well nest name, well name, and rmse
+                well_data.extend([rmse[rmse.index(i)-1], 
+                                  rmse[rmse.index(i)], 
+                                  rmse[rmse.index(i)+1]])
+                wellnest_list.extend([rmse[rmse.index(i)-1]])
+
+    # Unique well nests and locations only
+    unique = [];
+    
+    ## Getting rid of repeating wells and data points
+    # zip joins x and y coordinates in pairs
+    for x,y in zip(gwwell_locs.Long, gwwell_locs.Lat):
+        
+        # Check if x, y is unique
+        if (x, y) not in unique:
+            
+            # Saves this location for plotting
+            unique.append((x, y))
+            
+            # Label is well nest name
+            label = gwwell_locs.loc[gwwell_locs.Long == x]['WellNest_Name'].tolist()
+            # Specific well nest does not have a well in the aquifer
+            if label[0] not in wellnest_list:
+                continue
+                
+            # If well nest has a well in the aquifer
+            else:
+                
+                # Saving data
+                xs.append(x); ys.append(y); 
+                
+                # Relative RMSE multiplied by 100 and saved as %
+                cs.append(well_data[well_data.index(label[0])+2]*100)    
+      
+    # Creates a dictionary with location and relative RMSE value
+    d_dict[aq] = pd.DataFrame({"x": xs, "y": ys, "cs": cs})
+
+# Initializing figure
+fig, ax =  plt.subplots(figsize=(3.2, 2.2), dpi=300)
+
+## Plotting settings
+# Setting the same color bar limits for all four wells
+# Setting relative RMSE as a percentage; hence multiplying by 100
+data_temp = np.multiply([e for e in rmse if isinstance(e, float)], 100)
+data_lim = [min(np.array(data_temp)[~js_mfs.is_outlier(data_temp, 3.5)]),
+            max(np.array(data_temp)[~js_mfs.is_outlier(data_temp, 3.5)])]
+
+plt.set_cmap("coolwarm") # Color map settings
+
+## Plots
+# Creating a basemap
+map = Basemap(llcrnrlon=100.3,llcrnrlat=13.4,urcrnrlon=100.8,urcrnrlat=14,
+              resolution='h', ellps='WGS84', lat_0 = 13.6, lon_0 = 100.4)
+
+js_mfs.draw_basemap(map, xs, ys, d_dict, fig=fig, ax=ax, 
+                    datalim=data_lim, mode='RMSE_full', save=0, 
+                    aq=aq, perc=0,
+                    time_min=time_min, time_max=time_max, figpath=fig_path)
+
+
+## Saving figure
+fig_name1 = 'Paper_RMSE_' + time_min + '_' + time_max + '.png'
+full_figpath = os.path.join(fig_path, fig_name1)
+plt.savefig(full_figpath, bbox_inches='tight', format='png')
+
+#%%############################################################################
+# Relative RMSE Plotting
+###############################################################################
+# One graph: 2D plot with color indicator of tmax for all 4 aq
+
+# Importing spatial coordinates
+full_path = os.path.join(tot_path, 'GroundwaterWellLocs.xls')
+gwwell_locs = pd.read_excel(full_path)
+
+# Locations of wellnests; removing duplicates
+gwwell_locs = gwwell_locs.drop_duplicates('WellNest_Name', keep='first')
+
+# Aquifer of interest
+aqs = ["BK", "PD", "NL", "NB"]
+
+## Preallocation
+# Empty dictionary
+d_dict = {}
+
+# Getting data 
+# For each aquifer of interest
+for aq in aqs:
+    
+    ## Preallocation
+    # Saving relevant xs, ys, and tmax
+    xs = []; ys = []; cs = [];
+    # Subsets of the res_tmax data, and list of wellnest names
+    well_data = []; wellnest_list = [];
+    
+    ## Extracting relavent data
+    # Going through each res_tmax data
+    for i in res_tmax:
+        
+        # If string (well nest or aquifer name)
+        if type(i) == str:
+            
+            # If the aquifer name
+            if i.startswith(aq):
+                
+                # Saving well nest name, well name, and res_tmax
+                well_data.extend([res_tmax[res_tmax.index(i)-1], 
+                                  res_tmax[res_tmax.index(i)], 
+                                  res_tmax[res_tmax.index(i)+1]])
+                wellnest_list.extend([res_tmax[res_tmax.index(i)-1]])
+
+    # Unique x, ys only
+    # Dissuades if multiple well nests at the same location
+    unique = [];
+    
+    ## Getting rid of repeating wells and data points
+    # zip joins x and y coordinates in pairs
+    for x,y in zip(gwwell_locs.Long, gwwell_locs.Lat):
+        
+        # Check if x, y is unique
+        if (x, y) not in unique:
+            
+            # Saves location for plotting
+            unique.append((x, y))
+            
+            # Label is well nest name
+            label = gwwell_locs.loc[gwwell_locs.Long == x]['WellNest_Name'].tolist()
+            
+            # Specific well nest does not have a well in the aquifer
+            if label[0] not in wellnest_list:
+                continue
+                
+            # If well nest has a well in the aquifer
+            else:
+                
+                # Saving data
+                xs.append(x); ys.append(y); 
+                cs.append(well_data[well_data.index(label[0])+2])    
+                
+    # Dictionary with locations and data
+    d_dict[aq] = pd.DataFrame({"x": xs, "y": ys, "cs": cs})
+  
+## Plot settings
+# Initializing figure
+fig, ax =  plt.subplots(figsize=(3.2, 2.2), dpi=300)
+
+# Setting the same color bar limits for all four wells
+data_temp = [e for e in res_tmax if isinstance(e, float)]
+data_lim = [min(np.array(data_temp)[~js_mfs.is_outlier(data_temp, 3.5)]),
+            max(np.array(data_temp)[~js_mfs.is_outlier(data_temp, 3.5)])]
+
+plt.set_cmap("plasma") # Color map colors
+
+## Plots
+map = Basemap(llcrnrlon=100.3,llcrnrlat=13.4,urcrnrlon=100.8,urcrnrlat=14,
+              resolution='h', ellps='WGS84', lat_0 = 13.6, lon_0 = 100.4)
+js_mfs.draw_basemap(map, xs, ys, d_dict, fig=fig, ax=ax, 
+                    datalim=data_lim, mode='step_full', save=0, 
+                    aq=aq, perc=0,
+                    time_min=time_min, time_max=time_max, figpath=fig_path)
+
+## Saving Figures
+fig_name1 = 'Paper_tmax_' + time_min + '_' + time_max + '.png'
+full_figpath = os.path.join(fig_path, fig_name1)
+plt.savefig(full_figpath, bbox_inches='tight', format='png')
